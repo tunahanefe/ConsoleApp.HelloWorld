4cb42bbae5a99274fac9d10cb2d0740bbe49f4dd
TunahanEFE <TunahanEFE@TUNAHAN-EFE>
2022-06-06 13:38:17 +0300

    - Test için yeni proje oluşturuldu.

---
+++ ConsoleApp.HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bu HelloWorld uygulaması, GitHub test projesidir.");
+            Console.ReadKey();
+        }
+    }
+}
